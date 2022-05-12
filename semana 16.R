5bb7884072ea913d22124e9cb3d5d41b846a231f
Martin <martinzamarripa266@gmail.com>
2022-05-12 08:34:50 -0500

    Actualizar

---
+++ semana 16.R
@@ -0,0 +1,38 @@
+#MZZ
+#Semana 16
+#Examen 
+
+
+geiser <- read.csv("Clases/erupciones.csv")
+
+plot(geiser$waiting, geiser$eruptions, pch=19, xlab = "Tiempo de espera entre erupciones(min)", ylab = "Duracion de la erupcion (min)", col= "Red", main= "Geyser old Faithfull")
+
+#Errupcion 
+mean(geiser$eruptions)
+sd (geiser$eruptions)
+var(geiser$eruptions)
+
+#Waiting
+mean(geiser$waiting)
+sd(geiser$waiting)
+var(geiser$waiting)
+
+cor.test(geiser$waiting, geiser$eruptions)
+
+Geyser<- lm(geiser$eruptions ~ geiser$waiting)
+summary(Geyser)
+
+geiser$yprima <- round(-1.874016 + 0.075628* geiser$waiting,2)
+plot(geiser$waiting, geiser$eruptions, pch=19, xlab = "Tiempo de espera entre erupciones(min)", ylab = "Duracion de la erupcion (min)", col= "Red", main= "Geyser old Faithfull")
+abline(Geyser, col= "blue")
+text(85, 3, "y=-1.874 + 0.075*x")
+text(85, 2.5, "Regresion lineal")
+
+
+Geyser$coefficients
+sum(Geyser$residuals)
+
+
+
+valor <- c(80, 40, 45, 53, 61)
+-1.874016 + 0.075628* valor
