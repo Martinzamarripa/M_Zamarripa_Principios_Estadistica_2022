24b85bedeb18edfb66f9adc6ceb855d14ae3a337
Martin <martinzamarripa266@.com>
2022-05-20 21:14:08 -0500

    Semana 3

--- clases/Semana 3.R
+++ clases/Semana 3.R
@@ -3,46 +3,40 @@
 #Sesion semana 3 sala de computo 
 
 
+# Muestra basica de R -----------------------------------------------------
 
-# #muestra basica de R ----------------------------------------------------
-
-#Operacion basicas en R
-
+#Kenny Rincon
+# Operaciones basicas de R 
 2 + 2
-8 / 8
+8 / 4
 
-9 + 5 + 8 + 4
+# Agregar un objeto 
+resultado = 9 + 5 + 8 + 4
+resultado + resultado 
 
-resultado <- 9 + 5 + 8 + 4
+altura  <- c(1.67, 1.62, 1.62, 1.73, 1.58, 1.60, 1.70, 1.52, 1.74, 1.74)
 
-resultado + resultado
+# dividir cada altura entre 2
+altura / 2
 
-
-altura <- c (1.67, 1.62, 1.62, 1.73, 1.58, 1.60, 1.70, 1.52, 1.74, 1.74)
-
-altura
-
-#dividir cada altutra entre 2
-
-
-res.alt <- altura / 2
- res.alt
- 
- 
+# Graficas ---------------------------------------------------------------
 
 # Primera base de datos ---------------------------------------------------
 
-Ba <- c(1:5)
-h <- c(12, 13.1, 14.8, 16.3, 15.3)
+Ba <-  c(1:5)  
+h <- c(12, 13.1, 14.5, 16.3, 15.3)
 DAP <- c(20, 21.2, 22.1, 24.7, 22.7)
-DC<-(9, 8.5, 7.6, 9.3, 9.2)
-t <- c(15, 16, 15, 18, 17)
-Sp <- c("Pino", "Pino", "Pino", "Encino", "Encino")
+DC <- c(9, 8.5, 7.6, 9.3, 9.2)
+t <-c(15, 16, 15, 18,17)
+Sp <- c("Pino", "Pino","Pino", "Encino", "Encino")
 
 inventario <- data.frame(Ba, h, DAP, DC, t, Sp)
 
+mean(h)
+mean(inventario$DAP)
 
-
+# Agregar columna en inventario (dxh)
+inventario$dxh <- inventario$h * inventario$DAP
 
 
 
