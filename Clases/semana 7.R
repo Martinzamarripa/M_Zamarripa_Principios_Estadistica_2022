14f5e5a4b6af7ce62212813a4dd89f9d6a779f4c
Martin <martinzamarripa266@.com>
2022-05-21 01:08:17 -0500

    Borrar

--- Clases/semana 7.R
+++
@@ -1,9 +0,0 @@
-#Clase semana 8
-# Revisar longitud y ancho de las semillas 
-#Encino rojo 
-
-semillass <-read.csv("Clases/semillas.csv" , header = TRUE)
-
-
-mean(semillass$Longitud_mm)
-mean(semillass$Diametro_mm)
