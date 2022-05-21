31de0021cfe0a758c3b8b2097574de3413e7f3c2
Martin <martinzamarripa266@.com>
2022-05-20 21:42:31 -0500

    Update

--- Clases/Semana4.R
+++ Clases/Semana4.R
@@ -3,6 +3,7 @@
 #Descargar datos de internet
 
 
+
 url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfo.csv"
 
 #fileEncoding = "Latin1"
@@ -21,12 +22,14 @@ Est.C <- profepa [4:8,]
 Est.N <- profepa [17:18,]
 
 #obtener los estados con inspecciones > a 15 pero < a 35
+ins.media <- subset(profepa, profepa$Inspeccion >= 15 & profepa$Inspeccion<= 35)
+ins.media <-subset(profepa, profepa$Inspeccion >= 15 & profepa$Operativo >= 10)
 
-ins.media <- subset(profepa, profepa$Inspeccion >= 15 & profepa$Inspeccion >= 35)
+rec.media <-subset(profepa, profepa$Recorrido <= mean(profepa$Recorrido) & profepa$Operativo)
 
-ins.media2 <- subset(profepa, profepa$Inspeccion >= 15 & profepa$Operativo >= 10)
-ins.media3 <- subset(profepa, profepa$Recorrido<= mean(profepa$Recorrido) & profepa$Operativo<= men(profepa$Operativo))
+
 
 
 mean(profepa$Operativo)
 mean(profepa$Recorrido)
+
