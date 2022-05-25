bff0eec051f5915fab5c9c98ac3ad8d938075652
Martin <martinzamarripa266@.com>
2022-05-24 19:04:20 -0500

    actu

--- Clases/Semana6.R
+++ Clases/Semana6.R
@@ -4,10 +4,26 @@
 
 semillass <-read.csv("Clases/semillas.csv" , header = TRUE)
 
-#Habra diferencia en la germinacion ?
-#Habra diferencia en las alturas?
 
 
-boxplot(semillass$Peso_gr >40)
+graf <- boxplot(semillass$Peso_gr)
+
+which(semillass >40)
+semillass[524, ]
+
+
+quantile(semillass$Peso_gr, 0.0)
+graf$out
+
+
+
+# Identificar cuartiles ---------------------------------------------------
+
+
+quantile(semillass$Peso_gr, 0.25)
+quantile(semillass$Peso_gr, 0.5)
+quantile(semillass$Peso_gr, 0.75)
+quantile(semillass$Peso_gr, 1)
+
 
 
