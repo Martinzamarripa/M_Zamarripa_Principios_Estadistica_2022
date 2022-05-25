7fb1b8812ab754de9cc0bca8d2b7a03e61304b1b
Martin <martinzamarripa266@.com>
2022-05-25 08:07:04 -0500

    Actualizado

--- Clases/Semana8.R
+++ Clases/Semana8.R
@@ -1,26 +1,57 @@
-#Clase semana 8
-#Revisar datos de madera
-#BD maderas
-
-madera <- read.csv("Clases/madera.csv", header = TRUE)
+madera <- read.csv("Clases/madera.csv", header = T)
 
 madera$Sp <- as.factor(madera$Sp)
 
 boxplot(madera$Peso_gr ~ madera$Sp)
 
+library(dplyr)
+
+# alfa=0.05
+# Ho= la variable peso de la Sp barreta es igual al peso de la muestra teórica
+# H1= la variable peso de la Sp barreta es diferente 
+
+
+# Comparación de una media teórica Mu=8.0 para Sp=Barreta
+
 bar <- madera %>%
   filter(Sp=="Bar")
 
-#Comparacion de una media teorica Mu=8.0 para barreta
-
 mean(bar$Peso_gr)
 
+t.test(bar$Peso_gr, mu=8.0)
+t.test(bar$Peso_gr, mu=7.5)
+t.test(bar$Peso_gr, mu=7.45)
+t.test(bar$Peso_gr, mu=7.38)
 t.test(bar$Peso_gr, mu=7.4)
+
+# Comparación de una media teórica Mu=8.5 para Sp=Chp
 
 chp <- madera %>%
   filter(Sp=="Chp")
 
 mean(chp$Peso_gr)
-t.test(chp$Peso_gr, mu=7.6)
+t.test(chp$Peso_gr, mu=8.5)
 t.test(chp$Peso_gr, mu=7.4)
+t.test(chp$Peso_gr, mu=7.48)
+
+# Comparación de una media teórica Mu=12.00 para Sp=D.Eb
+
+d.eb <- madera %>%
+  filter(Sp=="D. Ebano")
+mean(d.eb$Peso_gr)
+t.test(d.eb$Peso_gr, mu=12)
+
+# Comparación de una media teórica Mu=11.3 para Sp=D.Eb
+t.test(d.eb$Peso_gr, mu=11.3)
+
+# Comparación de una media teórica Mu=8.3 para Sp=Eb
+
+Eb <- madera %>%
+  filter(Sp=="Ebano")
+
+mean(Eb$Peso_gr)
+t.test(Eb$Peso_gr, mu=8.3)
+
+# Comparación de una media teórica Mu=7.9 para Sp=Eb
+t.test(Eb$Peso_gr, mu=7.9)
 
